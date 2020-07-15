021774cc54038f72335663a7e0c0fcfac29f237c
Mouslim Aitssi <mouslimaitssi@gmail.com>
2020-07-15 14:09:18 +0100

    saving value in shared prefernces

--- app/src/main/java/com/example/tasbih/MainActivity.java
+++ app/src/main/java/com/example/tasbih/MainActivity.java
@@ -2,17 +2,21 @@ package com.example.tasbih;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button reset, increment;
     private TextView number;
     private long compteur = 0;
+    private SharedPreferences sp;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +25,14 @@ public class MainActivity extends AppCompatActivity {
         reset = findViewById(R.id.reset);
         increment = findViewById(R.id.increment);
         number = findViewById(R.id.number);
-
+        sp = getSharedPreferences("MyUserPrefs", MODE_PRIVATE);
+        loadResult();
         increment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 compteur++;
                 number.setText(String.valueOf(compteur));
+                saveResult();
             }
         });
 
@@ -35,7 +41,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 compteur = 0;
                 number.setText(String.valueOf(compteur));
+                saveResult();
             }
         });
+    }
+
+
+    public void saveResult() {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("result", number.getText().toString());
+        editor.commit();
+
+    }
+
+    public void loadResult() {
+        if (sp.contains("result")) {
+            compteur = Integer.parseInt(sp.getString("result", ""));
+            number.setText(String.valueOf(compteur));
+        }
     }
 }
